b7194a90deed41fd17d684a0c79628875f329ee4
Will McGugan <willmcgugan@gmail.com>
2022-02-13 10:21:35 +0000

    rich formula

---
+++ Formula/rich.rb
@@ -0,0 +1,90 @@
+class Rich < Formula
+    include Language::Python::Virtualenv
+  
+    desc "A command line toolbox for beautiful terminal output (syntax highlighting, markdown, CSV tables and more)"
+    homepage "https://github.com/textualize/Rich-CLI"
+    url "https://files.pythonhosted.org/packages/be/f3/052a62f2d0d2c27067bef88d26ad8eecba182c7490bc8d48dbc806f402b1/rich-cli-1.5.1.tar.gz"
+    sha256 "53c8087e432b0c9ecda83fac6c0173b0cb9995e2cfced8c3c17ce646b86a17a3"
+    license "MIT"
+    head "https://github.com/textualize/rich-cli.git", branch: "main"
+    
+    depends_on "python@3.10"
+  
+    resource "certifi" do
+      url "https://files.pythonhosted.org/packages/6c/ae/d26450834f0acc9e3d1f74508da6df1551ceab6c2ce0766a593362d6d57f/certifi-2021.10.8.tar.gz"
+      sha256 "78884e7c1d4b00ce3cea67b44566851c4343c120abd683433ce934a68ea58872"
+    end
+  
+    resource "charset-normalizer" do
+      url "https://files.pythonhosted.org/packages/56/31/7bcaf657fafb3c6db8c787a865434290b726653c912085fbd371e9b92e1c/charset-normalizer-2.0.12.tar.gz"
+      sha256 "2857e29ff0d34db842cd7ca3230549d1a697f96ee6d3fb071cfa6c7393832597"
+    end
+  
+    resource "click" do
+      url "https://files.pythonhosted.org/packages/f4/09/ad003f1e3428017d1c3da4ccc9547591703ffea548626f47ec74509c5824/click-8.0.3.tar.gz"
+      sha256 "410e932b050f5eed773c4cda94de75971c89cdb3155a72a0831139a79e5ecb5b"
+    end
+  
+    resource "colorama" do
+      url "https://files.pythonhosted.org/packages/1f/bb/5d3246097ab77fa083a61bd8d3d527b7ae063c7d8e8671b1cf8c4ec10cbe/colorama-0.4.4.tar.gz"
+      sha256 "5941b2b48a20143d2267e95b1c2a7603ce057ee39fd88e7329b0c292aa16869b"
+    end
+  
+    resource "commonmark" do
+      url "https://files.pythonhosted.org/packages/60/48/a60f593447e8f0894ebb7f6e6c1f25dafc5e89c5879fdc9360ae93ff83f0/commonmark-0.9.1.tar.gz"
+      sha256 "452f9dc859be7f06631ddcb328b6919c67984aca654e5fefb3914d54691aed60"
+    end
+  
+    resource "docutils" do
+      url "https://files.pythonhosted.org/packages/57/b1/b880503681ea1b64df05106fc7e3c4e3801736cf63deffc6fa7fc5404cf5/docutils-0.18.1.tar.gz"
+      sha256 "679987caf361a7539d76e584cbeddc311e3aee937877c87346f31debc63e9d06"
+    end
+  
+    resource "idna" do
+      url "https://files.pythonhosted.org/packages/62/08/e3fc7c8161090f742f504f40b1bccbfc544d4a4e09eb774bf40aafce5436/idna-3.3.tar.gz"
+      sha256 "9d643ff0a55b762d5cdb124b8eaa99c66322e2157b69160bc32796e824360e6d"
+    end
+  
+    resource "Pygments" do
+      url "https://files.pythonhosted.org/packages/94/9c/cb656d06950268155f46d4f6ce25d7ffc51a0da47eadf1b164bbf23b718b/Pygments-2.11.2.tar.gz"
+      sha256 "4e426f72023d88d03b2fa258de560726ce890ff3b630f88c21cbb8b2503b8c6a"
+    end
+  
+    resource "requests" do
+      url "https://files.pythonhosted.org/packages/60/f3/26ff3767f099b73e0efa138a9998da67890793bfa475d8278f84a30fec77/requests-2.27.1.tar.gz"
+      sha256 "68d7c56fd5a8999887728ef304a6d12edc7be74f1cfa47714fc8b414525c9a61"
+    end
+  
+    resource "rich" do
+      url "https://files.pythonhosted.org/packages/72/de/b3a53cf1dfdbdc124e8110a60d6c6da8e39d4610c82491fc862383960552/rich-11.2.0.tar.gz"
+      sha256 "1a6266a5738115017bb64a66c59c717e7aa047b3ae49a011ede4abdeffc6536e"
+    end
+  
+    resource "rich-cli" do
+      url "https://files.pythonhosted.org/packages/be/f3/052a62f2d0d2c27067bef88d26ad8eecba182c7490bc8d48dbc806f402b1/rich-cli-1.5.1.tar.gz"
+      sha256 "53c8087e432b0c9ecda83fac6c0173b0cb9995e2cfced8c3c17ce646b86a17a3"
+    end
+  
+    resource "rich-rst" do
+      url "https://files.pythonhosted.org/packages/29/0a/600b7f20120bcb27fcbf74298777240201733c92e806d53ce210844321eb/rich-rst-1.1.5.tar.gz"
+      sha256 "b0a9c0a0ae98a278b4e52ae07b4af56c0da3c3bbfa97be3c9a4b9fe28b3b7a89"
+    end
+  
+    resource "textual" do
+      url "https://files.pythonhosted.org/packages/fb/98/397820b98ed820e9b5a6da552cf9d68f73b04d4c141508b15c5081bf98f6/textual-0.1.15.tar.gz"
+      sha256 "61367cb7cf0dc0e68d3e41c54916d8170f57f50a4705bd407a9feb479873146e"
+    end
+  
+    resource "urllib3" do
+      url "https://files.pythonhosted.org/packages/b0/b1/7bbf5181f8e3258efae31702f5eab87d8a74a72a0aa78bc8c08c1466e243/urllib3-1.26.8.tar.gz"
+      sha256 "0e7c33d9a63e7ddfcb86780aac87befc2fbddf46c58dbb487e0855f7ceec283c"
+    end
+  
+    def install
+      virtualenv_install_with_resources
+    end
+  
+    test do
+      assert_match version._to_s, shell_output("#{bin}/rich --version")
+    end
+  end
